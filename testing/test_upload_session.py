e5d534de5df347995cce97a6057c44cc70f1b042
MortalWombat-repo <69204832+MortalWombat-repo@users.noreply.github.com>
2025-07-07 14:52:50 +0200

    Update test_upload_session.py

--- testing/test_upload_session.py
+++ testing/test_upload_session.py
@@ -2,21 +2,14 @@ import requests
 from pathlib import Path
 
 url = "http://localhost:8001/upload"
-<<<<<<< HEAD
 file_path = Path("/home/newuser/Downloads/contract_template.pdf")
-=======
-file_path = Path("/workspaces/69204832/Document_QA_with_FAISS-main/test_folder/contract_sample.pdf")
->>>>>>> f32d650 (Proper project root: move contents to repo root)
 
 if not file_path.exists():
     print(f"Error: File {file_path} does not exist on the host.")
     exit(1)
 
 data = {
-<<<<<<< HEAD
     "user_id": "123",
-=======
->>>>>>> f32d650 (Proper project root: move contents to repo root)
     "session": "true"
 }
 
@@ -25,8 +18,4 @@ with open(file_path, "rb") as f:
     response = requests.post(url, data=data, files=files)
 
 print("Status Code:", response.status_code)
-<<<<<<< HEAD
 print("Response:", response.json())
-=======
-print("Response:", response.json())
->>>>>>> f32d650 (Proper project root: move contents to repo root)
