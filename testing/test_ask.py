c62e8763dbfd9048284198e0e356f6de3efd5a74
MortalWombat-repo <69204832+MortalWombat-repo@users.noreply.github.com>
2025-07-07 14:50:06 +0200

    Update test_ask.py

--- testing/test_ask.py
+++ testing/test_ask.py
@@ -2,23 +2,12 @@ import requests
 
 url = "http://localhost:8002/ask"
 data = {
-<<<<<<< HEAD
-    "user_query": "ANTITRUST LAWS",
-    "user_language": "en",
-    "top_k": 1
-}
-
-response = requests.post(url, json=data)
-print(response.json())
-=======
     "user_query": "The invoices must contain which information?",
     "user_language": "en",
     "top_k": 1,
     "user_id": "123",
     "session_id": "manual"
-
 }
 
 response = requests.post(url, json=data)
 print(response.json())
->>>>>>> f32d650 (Proper project root: move contents to repo root)
